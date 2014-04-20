12edece294982a27c5a38f3cefe2ff914343d9e5
ulrich <git@myvdr.de>
2014-04-20 14:03:14 +0200

    can-light-control: Fix typo in config.h

--- can-light-control/config.h
+++ can-light-control/config.h
@@ -9,8 +9,8 @@
 ------------------------------------------------------------------------------*/
 
 #ifndef _CONFIG_H_
-	#define _CONFIG_H_	
-	
+	#define _CONFIG_H_
+
 	//ENOCEAN
 	#define USE_ENOCEAN 1
 
@@ -18,25 +18,22 @@
 	#define USE_HMI 1
 
 	//ENOCEANPARSER
-	#define USE_ENOCEANPARSER 0
+	#define USE_ENOCEAN_PARSER 0
 
 	//SHACKBUS
 	#define USE_SHACKBUS 1
 
 	//Taktfrequenz
 	//#define F_CPU 20000000UL
-	//#define F_CPU 8000000UL	
+	//#define F_CPU 8000000UL
 	#define F_CPU 14745600UL
 	//#define F_CPU 8000000UL
 	//#define F_CPU 11059200UL
-	
+
 	//Timertakt intern oder extern
 	#define EXTCLOCK 0 //0=Intern 1=Externer Uhrenquarz
 
 	//Baudrate der seriellen Schnittstelle
 	#define BAUDRATE 9600
-	
-	
+
 #endif //_CONFIG_H
-
-
