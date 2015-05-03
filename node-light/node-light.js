ec793816c9662838b54b27fc8f048e6fdf3bf3cc
ulrich <git@myvdr.de>
2015-05-03 21:09:49 +0200

    node: Add winston.Logger

--- node-light/node-light.js
+++ node-light/node-light.js
@@ -4,9 +4,24 @@ var nconf = require('nconf');
 var express = require('express');
 var app = express();
 
+require('mkdirp').mkdirp(__dirname + '/log')
+
 nconf.file({file: require('path').resolve(__dirname, 'storage.json') });
 var light_state = nconf.get('light_state');
 var power_state = nconf.get('power_state');
+
+
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.Console)({colorize: 'all'}),
+		new winston.transports.DailyRotateFile({
+			filename: __dirname + '/log/logfile.log',
+			datePattern: '.dd-MM-yyyy'
+		})
+	]
+});
 
 
 app.use(require('body-parser').json());
@@ -14,7 +29,7 @@ app.use(require('body-parser').json());
 app.all('*', function(req, res, next) {
 	res.set({'Content-Type': 'application/json; charset=utf-8', 'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'});
 	res.set({'Access-Control-Allow-Origin': '*'});
-	console.log(req.method + ' ' + req.url);
+	logger.info(req.method + ' ' + req.url);
 	next();
 });
 
@@ -29,7 +44,7 @@ app.get(/^\/lounge\/([1-8])$/, function(req, res) {
 });
 app.put(/^\/lounge\/([1-8])$/, function(req, res) {
     res.end();
-	console.log('state='+ req.body.state);
+	logger.info('state='+ req.body.state);
     sendUDP(req.params[0],req.body.state);
 });
 
@@ -38,7 +53,7 @@ app.get(/^\/power\/([1-5])$/, function(req, res) {
 });
 app.put(/^\/power\/([1-5])$/, function(req, res) {
     res.end();
-	console.log('state='+ req.body.state);
+	logger.info('state='+ req.body.state);
     sendUDPPower(req.params[0],req.body.state);
 });
 
@@ -50,20 +65,20 @@ app.options('*', function(req, res) {
 
 app.all('*', function(req, res) {
 	res.send(405, 'Method not allowed');
-	console.log('/: 405');
+	logger.info('/: 405');
 });
 
 app.listen(8080);
-console.log('Server started on port 8080');
+logger.info('Server started on port 8080');
 
 
 
 function exitHandler(options, err) {
-	console.log('\n Start exithandler()');
+	logger.info('\n Start exithandler()');
 	nconf.set('light_state',light_state);
 	nconf.set('power_state',power_state);
 	nconf.save();
-	console.log('successfull Good Bye');
+	logger.info('successfull Good Bye');
     if (options.exit) process.exit();
 }
 //catches ctrl+c event
@@ -83,23 +98,23 @@ socket.on('message', function (data) {
 
 socket.on("listening", function () {
   var address = socket.address();
-  console.log("server listening " + address.address + ":" + address.port);
+  logger.info("server listening " + address.address + ":" + address.port);
 });
 
 socket.bind(2342, function() {
-  console.log('server listing on 2342');
+  logger.info('server listing on 2342');
 });
 
 
 receiveUDP = function (data) {
-        console.log('receiveUDP Data...');
+        logger.info('receiveUDP Data...');
 
 	light_lut = new Array(1,2,3,4,6,7,8,5);
 	light_lut_state = new Array('off','on');
 
 	if (data.length == 2 && data[0] <= light_lut.length && data[1] <= 1) {
 		light_state[(light_lut[(data[0])])].state = light_lut_state[(data[1])];
-		console.log('receiveUDP: ' + data[0] + ' => ' + light_lut[(data[0])] + ' ' + data[1] + ' => ' + light_lut_state[(data[1])] );
+		logger.info('receiveUDP: ' + data[0] + ' => ' + light_lut[(data[0])] + ' ' + data[1] + ' => ' + light_lut_state[(data[1])] );
 	}
 
 	power_lut = new Array(120,140,141,142,143);
@@ -107,23 +122,23 @@ receiveUDP = function (data) {
 
 	if (data.length == 2 && data[0] == 10 && data[1] <= 1) {
 		power_state[1].state = power_lut_state[(data[1])];
-		console.log('receiveUDP: ' + data[0] + ' => ' + '120' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
+		logger.info('receiveUDP: ' + data[0] + ' => ' + '120' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
 	}
 	if (data.length == 2 && data[0] == 140 && data[1] <= 1) {
 		power_state[2].state = power_lut_state[(data[1])];
-		console.log('receiveUDP: ' + data[0] + ' => ' + '140' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
+		logger.info('receiveUDP: ' + data[0] + ' => ' + '140' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
 	}
 	if (data.length == 2 && data[0] == 141 && data[1] <= 1) {
 		power_state[3].state = power_lut_state[(data[1])];
-		console.log('receiveUDP: ' + data[0] + ' => ' + '141' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
+		logger.info('receiveUDP: ' + data[0] + ' => ' + '141' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
 	}
 	if (data.length == 2 && data[0] == 142 && data[1] <= 1) {
 		power_state[4].state = power_lut_state[(data[1])];
-		console.log('receiveUDP: ' + data[0] + ' => ' + '142' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
+		logger.info('receiveUDP: ' + data[0] + ' => ' + '142' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
 	}
 	if (data.length == 2 && data[0] == 143 && data[1] <= 1) {
 		power_state[5].state = power_lut_state[(data[1])];
-		console.log('receiveUDP: ' + data[0] + ' => ' + '143' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
+		logger.info('receiveUDP: ' + data[0] + ' => ' + '143' + ' ' + data[1] + ' => ' + power_lut_state[(data[1])] );
 	}
 
 }
@@ -149,7 +164,7 @@ sendUDP = function (id, state) {
 
 	if (state_old != -1) {
 		var packet = new Buffer([0xA5,0x5A,id_old,state_old]);
-		client.send(packet, 0, 4, 1337, 'licht.shack', function(err, bytes) { console.log(err) });
+		client.send(packet, 0, 4, 1337, 'licht.shack', function(err, bytes) { logger.info(err) });
 	}
 }
 
@@ -162,7 +177,7 @@ sendUDPPower = function (id, state) {
 	if (id == 4) id_eltako = 142;
 	if (id == 5) id_eltako = 143;
 
-	console.log('sendUDP: ' + state);
+	logger.info('sendUDP: ' + state);
 	if (typeof(state) == 'undefined') {}	
 	else if (state == 'on')  { state_old = 1; }
 	else if (state == 'off') { state_old = 0; }
@@ -172,7 +187,7 @@ sendUDPPower = function (id, state) {
 
 	if (state_old != -1) {
 		var packet = new Buffer([0xA5,0x5A,id_eltako,state_old]);
-		client.send(packet, 0, 4, 1337, 'licht.shack', function(err, bytes) { console.log(err) });
+		client.send(packet, 0, 4, 1337, 'licht.shack', function(err, bytes) { logger.info(err) });
 	}
 }
 
